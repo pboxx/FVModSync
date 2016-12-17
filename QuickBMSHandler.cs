48d115bb0e448d42d40fd6d8a3b65cbb4f977c60
pboxx <pbox@unera.se>
2016-12-17 14:30:59 +0100

    Implement warnings on conflicting CSV records from mods

--- QuickBMSHandler.cs
+++ QuickBMSHandler.cs
@@ -10,11 +10,9 @@ namespace FVModSync
     {
         public static void Unpack(string PakName, string ExportFolderName)
         {
-            // unpack cfg.pak in case user doesn't have it unpacked already
+            // unpack .pak in case user doesn't have it unpacked already
             // TODO find less pedestrian way (than just "if dir exists") to determine whether those exports are actually valid
-
             // TODO error catching if something goes wrong here (like cry if pak not found)
-            // oldversion: Process quickbms = Process.Start(@"quickbms\quickbms.exe", @"-F *.csv -o -q -Y -Q quickbms\life_is_feudal.bms ..\cfg.pak " + ExportFolder);
 
             Process quickbms = new Process();
             quickbms.StartInfo.FileName = @"quickbms\quickbms.exe";
